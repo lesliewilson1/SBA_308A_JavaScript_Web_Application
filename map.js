ee3b39c370b87b157eaea18d36aa7a70c50f32d5
Leslie <wilson.leslie2021@gmail.com>
2025-09-28 22:03:19 -0400

    Update 2.0

---
+++ map.js
@@ -0,0 +1,8 @@
+
+document.querySelectorAll('svg path[data-name]').forEach(function(label) {
+    label.addEventListener('click', function() {
+        const statename= label.dataset.name;
+        console.log(`clicked ${statename}`);
+    });
+});
+
