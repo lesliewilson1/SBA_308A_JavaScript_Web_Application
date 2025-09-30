fab7417d140a1fa6ce87286c8dd7382f7a21b6eb
Leslie <wilson.leslie2021@gmail.com>
2025-09-30 01:45:25 -0400

    Update 3.1

--- images.js
+++ images.js
@@ -1,5 +1,45 @@
 const api = 'https://api.restful-api.dev/objects';
 
-export function dicebearurl(stateName) {
+//testing to see if api functions
+
+// async function fetchApi() {
+//     try {
+//         const response = await fetch(api);
+//         if (!response.ok) {
+//             throw new Error(`Error: ${response.status}`);
+//         }
+//         const data = await response.json();
+//         console.log(data);
+//     }   catch (error) {
+//         console.log('Error fetching data', error)
+//     }
+// }
+
+// fetchApi();
+
+//------------------------------------------Generate profile pic based on state name-----------------------------------------------------//
+
+function getAvatar(state) {
     return `https://api.dicebear.com/6.x/pixel-art/svg?seed=${encodeURIComponent(stateName)}`;
 }
+
+async function postAvatar(state) {
+    const avatar = getAvatar(state);
+    let invalid = null;
+
+    try {
+        const response = await fetch (api);
+        const data = await response.json();
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].name === state) {
+                invalid = data[i];
+                break;
+            }
+        }
+    }  catch (error) {
+        console.log('Error fetching data', error)
+    }
+ 
+}
+
--- map.js
+++ map.js
@@ -1,8 +1,10 @@
+//Make states clickable by using foreach to loop through every data-name in svg
+
 export function stateClickHandlers(callback) {
     document.querySelectorAll('svg path[data-name]').forEach(function(label) {
     label.addEventListener('click', function() {
-        const statename= label.dataset.name;
-        console.log(`clicked ${statename}`);
+        const stateName= label.dataset.name;
+        console.log(`clicked ${stateName}`);
     });
 });
 
